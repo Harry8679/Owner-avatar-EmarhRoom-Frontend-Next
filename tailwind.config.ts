19dc65aafa1ff735f2c38b8b6b5610aaa1b79832
Harry EMARH <emarh.harry.code@gmail.com>
2025-03-23 23:59:58 +0100

    Tailwind config 2

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ module.exports = {
       "./pages/**/*.{js,ts,jsx,tsx,mdx}",
       "./components/**/*.{js,ts,jsx,tsx,mdx}",
       "./src/**/*.{js,ts,jsx,tsx,mdx}", // ✅ si tu utilises src
+      "./src/**/*.{js,ts,jsx,tsx,mdx}",
     ],
     darkMode: 'class',
     important: true,
